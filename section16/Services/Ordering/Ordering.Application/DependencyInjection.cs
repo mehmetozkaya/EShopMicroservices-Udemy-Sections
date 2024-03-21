e3121453699cf0569f58145f3e1be6635bf6edc6
mehmetozkaya <mehmetozkaya@gmail.com>
2024-03-21 14:45:58 +0300

    f

---
+++ section16/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using BuildingBlocks.Behaviors;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Ordering.Application;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddMediatR(config =>
+        {
+            config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+            config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+        });
+
+        return services;
+    }
+}
